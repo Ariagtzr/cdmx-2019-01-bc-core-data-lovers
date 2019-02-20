d03cdf5446771d9b8d622d445090f2cf784aad3d
Ariadna Gutierrez <ariadnagtzr@gmail.com>
2019-02-20 12:23:45 -0600

    conflic

--- src/data.js
+++ src/data.js
@@ -1,30 +1,26 @@
 //Objeto global
 window.WorldBank = {
   dataCountries: (data, countries) => { //Función para extraer la data por país. 
-  let dataByCountries = data[countries].indicators //Obtener los objetos por país.
-  return dataByCountries;
+    let dataByCountries = data[countries].indicators //Obtener los objetos por país.
+    return dataByCountries;
   },
-  percentAndYear:(data, matchYear) =>{ // Función para integrar al select los indicadores educativos.
-    let newData= {} //Crear objeto vacío para agregar la condición de búsqueda por palabra: educación.
+  percentAndYear: (data, matchYear) => { // Función para integrar al select los indicadores educativos.
+    let newData = {} //Crear objeto vacío para agregar la condición de búsqueda por palabra: educación.
     data.forEach(objeto => { //Recorre el objeto-data.
-      if(objeto.indicatorName === matchYear){ //Entra al objeto, busca el indicador por nombre y extrae la coincidencia.
+      if (objeto.indicatorName === matchYear) { //Entra al objeto, busca el indicador por nombre y extrae la coincidencia.
         newData = objeto.data //Incorpora a newData el nuevo objeto filtrado.
-        return newData 
+        return newData
       }
     });
     return newData
   },
-  sortPercentAndYear: (arrResultYear, sortValue) => {
-      if (sortValue === "upward") {
-        arrResultYear.sort((a,b)=>a[1]- b[1]);
-      } else if(sortValue === "falling") {
-        arrResultYear.sort((a,b)=>(b[1]-a[1]));
-      }
-    return arrResultYear  
+  sortPercentAndYear: (arrResultYear, sortValue) => { //Funcion para ordenar.
+    if (sortValue === "upward") {
+      arrResultYear.sort((a, b) => a[1] - b[1]);
+    } else if (sortValue === "falling") {
+      arrResultYear.sort((a, b) => (b[1] - a[1]));
+    }
+    return arrResultYear
+
   }
 };
-
-
-
-
-
